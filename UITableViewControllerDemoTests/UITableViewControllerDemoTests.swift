038a6351f738f7b6f2c22227a6fb51ddbe5f0f55
朱龙飞 <398670608@qq.com>
2015-08-21 15:18:47 +0800

    Initial Commit

---
+++ UITableViewControllerDemoTests/UITableViewControllerDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  UITableViewControllerDemoTests.swift
+//  UITableViewControllerDemoTests
+//
+//  Created by 朱龙飞 on 15/8/21.
+//  Copyright (c) 2015年 朱龙飞. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class UITableViewControllerDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
